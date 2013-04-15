bf8711353f6b2da76c72d69f6c30a096b8d790d8
Jay Wineinger <jay.wineinger@gmail.com>
2013-04-15 13:13:03 -0500

    Change multiformset.js to just define a function. pages need to call it to setup multiformset

--- multiformset/static/multiformset.js
+++ multiformset/static/multiformset.js
@@ -1,12 +1,23 @@
-$(function() {
-    var $chooser = $('#multiformset-form_chooser');
-    $(".form_template").each(function(idx, el) {
+function multiformset( options ) {
+
+    // Create some defaults, extending them with any options that were provided
+    var settings = $.extend( {
+        'template_selector' : '.form_template',
+        'add_form_selector' : '#multiformset-add_form',
+        'new_form_parent' : '#multiformset-new_forms',
+        'template_value_attr': 'data-form-class',
+        'template_text_attr': 'data-form-class'
+
+    }, options);
+
+    var $chooser = $(settings.add_form_selector);
+    $(settings.template_selector).each(function(idx, el) {
         var $el = $(el);
             $chooser.append($("<option></option>")
-                .attr("value", $el.attr('data-form-class'))
-                .text($el.attr('data-form-class')));
+                .attr("value", $el.attr(settings.template_value_attr))
+                .text($el.attr(settings.template_text_attr)));
     });
-    $("#multiformset-add_form").click(function(evt) {
+    $(settings.add_form_selector).click(function(evt) {
         // don't submit if this input happens to be within the form
         evt.preventDefault();
 
@@ -18,6 +29,7 @@ $(function() {
 
         // copy the template and replace prefixes with the correct index
         var html = $("#" + form_name + "-form_template").clone().html().replace(/__prefix__/g, qty);
-        $("#multiformset-new_forms").append(html);
+        $(settings.new_form_parent).append(html);
     });
-});
+
+}
