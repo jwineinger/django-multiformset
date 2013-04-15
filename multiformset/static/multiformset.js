a575dbbf521b5e5ee6bff7edbd3ef2f5e4f8fbe7
Jay Wineinger <jay.wineinger@gmail.com>
2013-04-15 09:13:14 -0500

    Split add.html into separate templates and static files

---
+++ multiformset/static/multiformset.js
@@ -0,0 +1,20 @@
+$(function() {
+    $(".form_template").each(function(idx, el) {
+        var $el = $(el);
+        $('#form_chooser')
+            .append($("<option></option>")
+                .attr("value", $el.attr('data-form-class'))
+                .text($el.attr('data-form-class')));
+    });
+    $("#multiformset-add_form").click(function() {
+        // update total form count
+        var form_name = $("#multiformset-form_chooser").val();
+        var $total_forms = $("#id_" + form_name + "-TOTAL_FORMS");
+        var qty = parseInt($total_forms.val());
+        $total_forms.val(qty + 1);
+
+        // copy the template and replace prefixes with the correct index
+        var html = $("#" + form_name + "-form_template").clone().html().replace(/__prefix__/g, qty);
+        $("#multiformset-new_forms").append(html);
+    });
+});
